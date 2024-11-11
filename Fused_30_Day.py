31276b7f73df4d4c9a5b5516a353e5b849a1645d
Maxime Lenormand <maximelenormand@maximes-mbp-2.home>
2024-11-11 18:10:19 +0100

    Typo + keeping track of latest link

--- Fused_30_Day.py
+++ Fused_30_Day.py
@@ -13,8 +13,8 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Fused is an end-to-end cloud platform for data analytics.
-    You can build run Python function on our serverless platform, host them as individual apps or pooint to
+    [Fused](https://www.fused.io/) is an end-to-end cloud platform for data analytics.
+    You can build run Python function on our serverless platform, host them as individual apps or point to
     them through HTTP endpoints. 
 
     This page is a little demo of all the maps we & the community have built using Fused during the [2024 #30DayMapChallenge](https://30daymapchallenge.com/)
